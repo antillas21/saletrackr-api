a1ce3aa3b5aa35e6181316734012fb9509a915e5
Antonio Antillon <antillas21@gmail.com>
2013-10-14 23:29:14 -0700

    Reconfigures Serializers to include root element.

--- config/initializers/serializers.rb
+++ config/initializers/serializers.rb
@@ -1,7 +1,7 @@
 ActiveSupport.on_load(:active_model_serializers) do
   # Disable for all serializers (except ArraySerializer)
-  ActiveModel::Serializer.root = false
+  # ActiveModel::Serializer.root = false
 
   # Disable for ArraySerializer
-  ActiveModel::ArraySerializer.root = false
+  # ActiveModel::ArraySerializer.root = false
 end
